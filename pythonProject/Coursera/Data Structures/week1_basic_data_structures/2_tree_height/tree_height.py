60369b8782e53b01ca21fbe166f488e36cb97027
Maksim Rudenko <maksim20014@hotmail.com>
2023-02-19 13:00:04 +0300

    Coursera

---
+++ pythonProject/Coursera/Data Structures/week1_basic_data_structures/2_tree_height/tree_height.py
@@ -0,0 +1,35 @@
+import sys
+import threading
+
+def compute_height_git(n, parents):
+    '''Выводит высоту дерева (правда как оно представляется не понятно. Пример вводных данных:
+    5
+    4 -1 4 1 1'''
+    T = [None] * n
+    root = parents.index(-1)
+    T[root] = 1
+    for vertex in range(n):
+        height = 0
+        current = vertex
+        while current != -1:
+            height += 1
+            current = parents[current]
+            if T[current]:
+                T[vertex] = T[current] + height
+                break
+
+    return max(T)
+
+
+def main():
+    n = int(input())
+    parents = list(map(int, input().split()))
+    print(compute_height_git(n, parents))
+
+
+# In Python, the default limit on recursion depth is rather low,
+# so raise it here for this problem. Note that to take advantage
+# of bigger stack, we have to launch the computation in a new thread.
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
+threading.Thread(target=main).start()
