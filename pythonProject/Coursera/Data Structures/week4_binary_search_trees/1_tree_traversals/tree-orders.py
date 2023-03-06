47bf8754d41c497fcc299b8cd2e667d1f4c8caf0
Maksim Rudenko <maksim20014@hotmail.com>
2023-03-06 13:12:17 +0300

    Coursera

---
+++ pythonProject/Coursera/Data Structures/week4_binary_search_trees/1_tree_traversals/tree-orders.py
@@ -0,0 +1,62 @@
+# from Git
+
+import sys, threading
+
+sys.setrecursionlimit(10 ** 6)  # max depth of recursion
+threading.stack_size(2 ** 27)  # new thread will get stack of such size
+
+
+class TreeOrders:
+    def read(self):
+        self.n = int(sys.stdin.readline())
+        self.key = [0 for i in range(self.n)]
+        self.left = [0 for i in range(self.n)]
+        self.right = [0 for i in range(self.n)]
+        for i in range(self.n):
+            [a, b, c] = map(int, sys.stdin.readline().split())
+            self.key[i] = a
+            self.left[i] = b
+            self.right[i] = c
+
+    def inOrder(self, root):
+        if root == -1:
+            return []
+        self.inOrder(self.left[root])
+        print(self.key[root], end=' ')
+        self.inOrder(self.right[root])
+
+    # Finish the implementation
+    # You may need to add a new recursive method to do that
+
+    def preOrder(self, root):
+        if root == -1:
+            return []
+        print(self.key[root], end=' ')
+        self.preOrder(self.left[root])
+        self.preOrder(self.right[root])
+
+    # Finish the implementation
+    # You may need to add a new recursive method to do that
+
+    def postOrder(self, root):
+        if root == -1:
+            return []
+        self.postOrder(self.left[root])
+        self.postOrder(self.right[root])
+        print(self.key[root], end=' ')
+    # Finish the implementation
+    # You may need to add a new recursive method to do that
+
+
+def main():
+    tree = TreeOrders()
+    tree.read()
+    tree.inOrder(0)
+    print()
+    tree.preOrder(0)
+    print()
+    tree.postOrder(0)
+    print()
+
+
+threading.Thread(target=main).start()
